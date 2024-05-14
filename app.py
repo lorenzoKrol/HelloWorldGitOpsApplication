84018c24cba4e1f70404f1ad9da9534ebfa0c124
lorenzoKrol <lorenzokrol2000@gmail.com>
2024-05-14 14:22:48 +0200

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello test cd 10!'
+    return 'Hello test ci add-on1!'
 
 
 if __name__ == '__main__':
