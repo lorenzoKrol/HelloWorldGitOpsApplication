e78799aeb84a6911c83329d197de631ba0e6b35a
lorenzoKrol <lorenzokrol2000@gmail.com>
2024-05-16 14:11:18 +0200

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello test cd composite 5'
+    return 'Hello bas'
 
 
 if __name__ == '__main__':
