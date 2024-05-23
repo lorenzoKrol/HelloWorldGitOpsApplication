4a3d9c47255c316ac1485ca40891fc8356472d40
lorenzoKrol <lorenzokrol2000@gmail.com>
2024-05-23 11:28:20 +0200

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello released based promotion + production 2'
+    return 'Hello released based promotion + production 3'
 
 
 if __name__ == '__main__':
