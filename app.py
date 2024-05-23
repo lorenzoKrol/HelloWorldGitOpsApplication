772efb8392806a232425505a713dd47769438bf4
lorenzoKrol <lorenzokrol2000@gmail.com>
2024-05-23 09:23:43 +0200

    remove metrics from ci

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello deprecation test'
+    return 'Hello metrics 1'
 
 
 if __name__ == '__main__':
