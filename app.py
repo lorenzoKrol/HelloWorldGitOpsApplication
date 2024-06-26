f099fe1db6082a3762a2c72f2a0a1d7d87f111dd
lorenzoKrol <42969133+lorenzoKrol@users.noreply.github.com>
2024-06-26 15:07:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Achmea OpenShift'
+    return 'Hello Graduation'
 
 
 if __name__ == '__main__':
