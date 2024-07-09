262075001c760d3b1243be49d84f6fbd36d22f61
lorenzoKrol <42969133+lorenzoKrol@users.noreply.github.com>
2024-07-09 10:26:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 ###
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Graduation!!!'
+    return 'Hello Diploma'
 
 
 if __name__ == '__main__':
